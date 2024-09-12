32a9f35ba958a39fddcd706a324f4cb1bb9f39e0
kinleyrabgay <fsd.rabgay@gmail.com>
2024-09-12 18:17:28 +0600

    app: vercel.json config
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
+  // App
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // CORs
+  const corsOptions: CorsOptions = {
+    origin: [`*`],
+    credentials: true,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    allowedHeaders: 'Authorization, Content-Type',
+  };
+
+  app.enableCors(corsOptions);
+
+  // Global Prefix
+  app.setGlobalPrefix('api/v1');
+
+  // Port
+  await app.listen(process.env.PORT);
 }
 bootstrap();
